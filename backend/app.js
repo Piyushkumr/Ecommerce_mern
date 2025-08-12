1e352f902d14cd1d075fd700e331edd7aaee010d
piyush <piyush7206@gmail.com>
2025-08-13 01:34:37 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,19 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path");
+const cors = require("cors");
 
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
-    require("dotenv").config({ path: 'backend/config/config.env' });
+  require("dotenv").config({ path: 'backend/config/config.env' });
 }
 
-app.use(express.json({limit: "50mb"}));
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}));
+
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 // app.use(fileUpload());
--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -11,10 +11,12 @@ const sendToken = (user, statusCode, res) => {
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
+        secure: true,
+        sameSite: "None",
     };
 
     res.status(statusCode).cookie('token', token, options).json({
-        success:true,
+        success: true,
         user,
         token,
     });
